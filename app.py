1f116b49635df74f55ef41010ff08756d46bfc73
Praneet9 <praneetbomma@gmail.com>
2019-04-12 00:12:42 +0530

    register template done

--- app.py
+++ app.py
@@ -1,30 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-posts = [
-    {
-        'author': 'Praneet Bomma',
-        'title': 'First Post',
-        'date_posted': 'April 8, 2019',
-        'content': 'This is my first post'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Second Post',
-        'date_posted': 'April 9, 2019',
-        'content': 'This is the second post'
-    }
-]
+app.config['SECRET_KEY'] = 'ae4a5462339fad4e4924c778ec41cd07'
+
+posts = [{
+    'author': 'Praneet Bomma',
+    'title': 'First Post',
+    'date_posted': 'April 8, 2019',
+    'content': 'This is my first post'
+},
+         {
+             'author': 'Jane Doe',
+             'title': 'Second Post',
+             'date_posted': 'April 9, 2019',
+             'content': 'This is the second post'
+         }]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form = form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form = form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
