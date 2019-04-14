a6fc266d8d160af9d83c91f46bf9a52b6fd5524f
Praneet9 <praneetbomma@gmail.com>
2019-04-14 23:17:31 +0530

    video 3 done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -30,15 +30,24 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for ' + form.username.data + '!', 'success')
+        return redirect(url_for('home')) 
     return render_template('register.html', title='Register', form = form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
     return render_template('login.html', title='Login', form = form)
 
 
