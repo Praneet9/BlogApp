169c702b98131662216e948952bc1e4a2bd7725a
Praneet9 <praneetbomma@gmail.com>
2019-05-16 22:30:32 +0530

    template inheritance and context done

--- Django_BlogApp/blog/views.py
+++ Django_BlogApp/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Praneet',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 15, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 16, 2019'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
