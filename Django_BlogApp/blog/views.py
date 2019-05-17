085530df72c05cef64969939036561c428087e7f
Praneet9 <praneetbomma@gmail.com>
2019-05-17 17:11:59 +0530

    database connected & 5th vid done

--- Django_BlogApp/blog/views.py
+++ Django_BlogApp/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Praneet',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 15, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 16, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
