aafc6f4efb417fe395d826e4c20e01a13fcdaed5
Praneet9 <praneetbomma@gmail.com>
2019-05-31 10:02:22 +0530

    login and logout system & 7th vid done

--- Django_BlogApp/users/views.py
+++ Django_BlogApp/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -8,9 +9,13 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for '+username+'!')
-            return redirect('blog-home')
+            messages.success(request, 'Your account has been created!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
