78778bdad5656925f80bcf83f9b3df45bfef82f0
Praneet9 <praneetbomma@gmail.com>
2019-06-21 08:06:00 +0530

    PostListView done

--- Django_BlogApp/blog/views.py
+++ Django_BlogApp/blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['date_posted']
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
