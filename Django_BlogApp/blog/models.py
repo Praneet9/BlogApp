27207ab1afd1e73cd82350c3e9d2e21b5bba03c8
Praneet9 <praneetbomma@gmail.com>
2019-06-21 10:21:50 +0530

    working on post update and delete

--- Django_BlogApp/blog/models.py
+++ Django_BlogApp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Post(models.Model):
@@ -10,3 +11,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- Django_BlogApp/blog/views.py
+++ Django_BlogApp/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
 
 def home(request):
     context = {
@@ -16,7 +23,37 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-    
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
