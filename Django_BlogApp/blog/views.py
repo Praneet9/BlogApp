5523ec51a484ec80ffb97dc59faf12bee53765bb
Praneet9 <praneetbomma@gmail.com>
2019-06-22 18:43:29 +0530

    post update and delete done

--- Django_BlogApp/blog/views.py
+++ Django_BlogApp/blog/views.py
@@ -48,12 +48,14 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
